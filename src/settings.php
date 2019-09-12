f33e84349265f8eedd396c58d21e9de72731da1d
RandomPerson1907 <random.person.1907@gmail.com>
2019-09-12 15:17:31 +0300

    Have added constant with categories page

--- src/settings.php
+++ src/settings.php
@@ -1,2 +1,3 @@
 <?php
-    define("SITE_TO_PARSE", "http://health-diet.ru/base_of_food/sostav/18812.php");
+    define("SITE_TO_PARSE", "http://health-diet.ru");
+    define("PAGE_WITH_CATEGORIES", "http://health-diet.ru/base_of_food/sostav/18812.php");
